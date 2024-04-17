6c0444a67e6372dd0ffe9fe7d0010e9bb3b68179
khrishchanovich <orannon@gmail.com>
2024-04-17 13:52:02 +0300

    Add some test

--- Lab4/Lab4.sql
+++ Lab4/Lab4.sql
@@ -1,77 +1,6 @@
 DROP TYPE xml_record;
 CREATE TYPE xml_record IS TABLE OF VARCHAR2(1000);
 
-DROP TABLE Students;
-DROP TABLE Groups;
-
-CREATE TABLE Groups (
-    id NUMBER PRIMARY KEY NOT NULL,
-    name VARCHAR2(20) NOT NULL,
-    c_val NUMBER DEFAULT 0 NOT NULL
-);
-
-CREATE TABLE Students (
-    id NUMBER PRIMARY KEY NOT NULL,
-    name VARCHAR2(20) NOT NULL,
-    group_id NUMBER NOT NULL
-);
-
-CREATE OR REPLACE TRIGGER update_students_value_in_groups 
-    AFTER INSERT OR UPDATE OR DELETE ON Students FOR EACH ROW
-BEGIN
-    IF INSERTING THEN
-        UPDATE Groups SET c_val = c_val + 1 WHERE id = :NEW.group_id;
-    ELSIF UPDATING THEN
-            UPDATE Groups SET c_val = c_val - 1 WHERE id = :OLD.group_id;
-            UPDATE Groups SET c_val = c_val + 1 WHERE id = :NEW.group_id;
-    ELSIF DELETING THEN
-            UPDATE Groups SET c_val = c_val - 1 WHERE id = :OLD.group_id;
-    END IF;
-END;
-
-DROP SEQUENCE id_auto_increment_for_groups;
-DROP SEQUENCE id_auto_increment_for_students;
-
-CREATE SEQUENCE id_auto_increment_for_groups 
-    START WITH 1 
-    INCREMENT BY 1 
-    NOMAXVALUE;
-
-CREATE SEQUENCE id_auto_increment_for_students 
-    START WITH 1 
-    INCREMENT BY 1 
-    NOMAXVALUE;
-
-CREATE OR REPLACE TRIGGER generate_students_id 
-    BEFORE INSERT ON Students FOR EACH ROW
-BEGIN
-    SELECT  id_auto_increment_for_students.NEXTVAL 
-        INTO :NEW.id FROM DUAL;
-END;
-
-CREATE OR REPLACE TRIGGER generate_groups_id 
-    BEFORE INSERT ON Groups FOR EACH ROW
-BEGIN
-    SELECT id_auto_increment_for_groups.NEXTVAL 
-        INTO :NEW.id FROM DUAL;
-END;
-
-INSERT INTO Groups(name) VALUES('001');
-INSERT INTO Groups(name) VALUES('002');
-INSERT INTO Groups(name) VALUES('003');
-
-INSERT INTO Students(name, group_id) VALUES('A', 1);
-INSERT INTO Students(name, group_id) VALUES('B', 2);
-INSERT INTO Students(name, group_id) VALUES('C', 2);
-INSERT INTO Students(name, group_id) VALUES('D', 1);
-INSERT INTO Students(name, group_id) VALUES('E', 2);
-INSERT INTO Students(name, group_id) VALUES('F', 1);
-INSERT INTO Students(name, group_id) VALUES('G', 1);
-INSERT INTO Students(name, group_id) VALUES('R', 3);
-
-SELECT * FROM Students;
-SELECT * FROM Groups;
-
 DECLARE
     v_sql VARCHAR2(4000);
     cur sys_refcursor;
@@ -245,4 +174,9 @@ BEGIN
  SELECT t1_pk_seq.nextval INTO :NEW.ID FROM dual; 
  END IF; 
  END IF; 
+END;
+
+
+BEGIN
+    DBMS_OUTPUT.put_line(xml_package.xml_delete(read('new_delete.xml')));
 END;
